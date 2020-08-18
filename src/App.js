27c59fcabb521aa98da7bd38effb0d70e3c51d21
PhatNguyen203 <Lee0ne203@gmail.com>
2020-08-17 20:32:21 -0400

    created Navigation and Logo components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello The World</h1>
+      <Navigation />
+      <Logo />
     </div>
   );
 }
